corrected mismatch in intrinsic vs extrinsic Euler rotation

common/animate.py
@@ -98,7 +98,7 @@ def plot_vehicle(ax, eta, box_size):
     """Draws a vehicle box and axes at pose eta."""
     pos = eta[:3]
     phi, theta, psi = eta[3:6]
-    rot = R.from_euler('zyx', [psi, theta, phi]).as_matrix()
+    rot = R.from_euler('ZYX', [psi, theta, phi]).as_matrix()
     l, w, h = box_size
     corners = np.array([
         [ l/2,  w/2, -h/2],
@@ -467,8 +467,8 @@ def plot_pose_error_boxplots(reference_eta, real_eta):
     # Attitude error norm (Euler angles, in radians)
     att_error = np.linalg.norm(reference_eta[:, 3:] - real_eta[:, 3:], axis=1)
     # Convert Euler angles to quaternions for all timesteps
-    reference_quat = R.from_euler('zyx', reference_eta[:, 5::-1][:, :3]).as_quat()  # [x, y, z, w]
-    real_quat = R.from_euler('zyx', real_eta[:, 5::-1][:, :3]).as_quat()
+    reference_quat = R.from_euler('ZYX', reference_eta[:, 5::-1][:, :3]).as_quat()  # [x, y, z, w]
+    real_quat = R.from_euler('ZYX', real_eta[:, 5::-1][:, :3]).as_quat()
 
     # Reorder to [w, x, y, z] for consistency with your functions
     reference_quat = np.concatenate([reference_quat[:, 3:4], reference_quat[:, :3]], axis=1)

common/utils_math.py
@@ -73,7 +73,7 @@ def rotation_matrix_from_quat(q):
 def rotation_matrix_from_euler(phi, theta, psi):
     """
     Create a rotation matrix from Euler angles (phi, theta, psi) = (roll, pitch, yaw).
-    ZYX extrinsic convention is used
+    ZYX intrinsic convention is used
     """
     # cphi = np.cos(phi)
     # sphi = np.sin(phi)
@@ -86,6 +86,24 @@ def rotation_matrix_from_euler(phi, theta, psi):
     #     [spsi * ctheta, spsi * stheta * sphi + cpsi * cphi, spsi * stheta * cphi - cpsi * sphi],
     #     [-stheta,        ctheta * sphi,                     ctheta * cphi]
     # ])
+    # Rx = np.array([
+    #     [1, 0, 0],
+    #     [0, np.cos(phi), -np.sin(phi)],
+    #     [0, np.sin(phi), np.cos(phi)]
+    # ])
+    # Ry = np.array([
+    #     [np.cos(theta), 0, np.sin(theta)],
+    #     [0, 1, 0],
+    #     [-np.sin(theta), 0, np.cos(theta)]
+    # ])
+    # Rz = np.array([
+    #     [np.cos(psi), -np.sin(psi), 0],
+    #     [np.sin(psi), np.cos(psi), 0],
+    #     [0, 0, 1]
+    # ])
+
+    # R1 = Rz @ Ry @ Rx
+
     R = Rot.from_euler('ZYX', [psi, theta, phi]).as_matrix() # case sensitive: upper case is extrinsic -> rotating in world frame
     return R
 